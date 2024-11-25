9ffa8cef1022d121ff4c0174138c7dc49eab0d24
aledubiel <151976227+aledubiel@users.noreply.github.com>
2024-11-25 20:09:05 -0300

    Add files via upload

--- Projeto_01-Ale/config.php
+++ Projeto_01-Ale/config.php
@@ -1,3 +1,12 @@
 <?php
+    session_start();
     define('INCLUDE_PATH', 'http://localhost/Projeto_01-Ale/');
+    define('INCLUDE_PATH_PAINEL', INCLUDE_PATH.'painel/');
+
+    $autoload = function($class){
+        include('assets/classes/'.$class.'.php');
+    };
+
+    spl_autoload_register($autoload)
 ?>
+
